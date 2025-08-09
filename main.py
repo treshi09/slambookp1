5c4c2c19d026323c1b4f1f392a4e5c68fa655f4a
Your Name <you@example.com>
2025-08-09 11:22:18 +0100

    commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome to My Slambook!")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+phone = input("What is your phone number? ")
+fav_color = input("What is your favourite colour? ")
+best_memory = input("What is your best memory with me? ")
+
+print("\nSLAMBOOK ENTRY")
+print("Name:", name)
+print("Age:", age)
+print("Phone:", phone)
+print("Favourite Colour:", fav_color)
+print("Best Memory:", best_memory)
