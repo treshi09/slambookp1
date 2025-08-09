0c5fb44021e6ec59279002fe04a98f0d77eb3efe
Your Name <you@example.com>
2025-08-09 11:27:22 +0100

    commit

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 print("Welcome to My Slambook!")
 
-name = input("What is your name? ")
-age = input("How old are you? ")
-phone = input("What is your phone number? ")
-fav_color = input("What is your favourite colour? ")
-best_memory = input("What is your best memory with me? ")
+slambook = []
 
-print("\nSLAMBOOK ENTRY")
-print("Name:", name)
-print("Age:", age)
-print("Phone:", phone)
-print("Favourite Colour:", fav_color)
-print("Best Memory:", best_memory)
+while True:
+    name = input("What is your name? ")
+    age = input("How old are you? ")
+    phone = input("What is your phone number? ")
+    fav_color = input("What is your favourite colour? ")
+    best_memory = input("What is your best memory with me? ")
+
+    entry = {
+        "Name": name,
+        "Age": age,
+        "Phone": phone,
+        "Favourite Colour": fav_color,
+        "Best Memory": best_memory
+    }
+
+    print("\nSLAMBOOK ENTRY")
+    print("Name:", name)
+    print("Age:", age)
+    print("Phone:", phone)
+    print("Favourite Colour:", fav_color)
+    print("Best Memory:", best_memory)
+
+    slambook.append(entry)
+
+    more = input("Do you want to add another entry? (yes/no): ").lower()
+    if more != "yes":
+        break
+
+print("\n--- ALL SLAMBOOK ENTRIES ---")
+for i, friend in enumerate(slambook, start=1):
+    print(f"\nFriend #{i}")
+    for key, value in friend.items():
+        print(f"{key}: {value}")
+print("\nThank you for filling my slambook")
